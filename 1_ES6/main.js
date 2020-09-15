e713b555485edef6dc8c53385ac41cca6584f42b
89P13 <fc.armando@outlook.com>
2020-09-14 23:20:28 -0500

    Funcion palindrome - minusculas

---
+++ 1_ES6/main.js
@@ -0,0 +1,12 @@
+//alert ("funciona");
+
+oracion = " ";
+function palindrome(oracion){
+   minustext = oracion.toLowerCase();
+   return minustext;
+}
+
+
+console.log(palindrome("Arriba la birra"));
+
+
