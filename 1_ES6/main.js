bd860102c389951e5176344ed768ee660f73f631
89P13 <fc.armando@outlook.com>
2020-09-15 00:40:06 -0500

    Funcion palindrome -comparación

--- 1_ES6/main.js
+++ 1_ES6/main.js
@@ -2,15 +2,26 @@
 
 //oracion = " ";
 function palindrome(oracion){
-   minustext = oracion.toLowerCase();
+    minustext = oracion.toLowerCase();
 //   return minustext;
-   espatext = minustext.split("");
-  return espatext;
-//   sinEspaText = espatext.replace(/ /g,"");
+    sinEspaText = minustext.replace(/ /g,"");
 //   return sinEspaText;
+    espatext = sinEspaText.split("");
+//    return espatext;
+    dereversa = sinEspaText.split("").reverse();
+//    return dereversa;
+    for(i in espatext){
+    if (dereversa[i] == espatext[i]){
+        resultado = true
+    } else{
+        resultado = false
+    }
+    console.log(resultado);
+  }
 }
 
 
-console.log(palindrome("Arriba la birra"));
+palindrome("Arriba la birra");
+palindrome("Arriba la cerveza");
 
 
